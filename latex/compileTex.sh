05a51939d3d69e19d668fee1cb417f89b07475c8
angelodel80 <angelodel80@gmail.com>
2020-05-16 19:21:55 +0200

    add latex structure

---
+++ latex/compileTex.sh
@@ -0,0 +1,4 @@
+#! bin/bash
+pdflatex main-seminario-girolamini.tex;
+pdflatex main-seminario-girolamini.tex;
+pdflatex main-seminario-girolamini.tex;
---
+++ latex/remove-files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm main-seminario-girolamini.aux; 
+rm main-seminario-girolamini.log; 
+rm main-seminario-girolamini.nav;
+rm main-seminario-girolamini.out; 
+rm main-seminario-girolamini.pdf; 
+rm main-seminario-girolamini.snm; 
+rm main-seminario-girolamini.toc;
